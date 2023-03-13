72899e61540ba26cb30661a0bd6578f53cc4db95
rootdobos <49080519+rootdobos@users.noreply.github.com>
2023-03-13 21:09:04 +0200

    added configuration to gradcam

--- sandbox/gradcam.py
+++ sandbox/gradcam.py
@@ -12,18 +12,38 @@ warnings.filterwarnings('ignore')
 
 
 class config:
-    seed = 2023
-    batch_size = 2
-    img_size = 128
-    num_tiles = 25
-    num_classes = 6
-    num_splits = 5
-    num_epochs = 5
-    learning_rate = 3e-3
-    num_workers = 1
-    verbose = True
-    backbone_train_path = 'tiles/128_5_5/'
-    #backbone_test_path = '../input/prostate-cancer-grade-assessment/test_images/'
+    def __init__(self, 
+            model_name,
+            seed ,
+            batch_size,
+            img_size ,
+            num_tiles,
+            num_classes,
+            num_splits ,
+            num_epochs ,
+            learning_rate,
+            num_workers ,
+            verbose ,
+            train_data_name,
+            last_conv_layer_name,
+            classifier_layer_names):
+        self.train_data_name=train_data_name
+        self.backbone_train_path='tiles/{}/'.format(train_data_name)
+        self.model_name=model_name
+        self.seed = seed
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.num_tiles = num_tiles
+        self.num_classes = num_classes
+        self.num_splits = num_splits
+        self.num_epochs = num_epochs
+        self.learning_rate = learning_rate
+        self.num_workers = num_workers
+        self.verbose = verbose
+        self.train_csv = "tiles/{}.csv".format(train_data_name)
+        self.last_conv_layer_name=last_conv_layer_name
+        self.classifier_layer_names=classifier_layer_names
+        #backbone_test_path = '../input/prostate-cancer-grade-assessment/test_images/'
 
 def seed_everything(seed):
     random.seed(seed)
@@ -36,7 +56,7 @@ def get_axis_max_min(array, axis=0):
     axis_max = len(one_axis) - next((i for i, x in enumerate(one_axis[::-1]) if x), 0)
     return axis_min, axis_max
 
-def get_img_array(img_path):
+def get_img_array(img_path, config):
     img = tf.keras.preprocessing.image.load_img(img_path, target_size=(config.img_size, config.img_size))
     array = tf.keras.preprocessing.image.img_to_array(img)
     array = np.expand_dims(array, axis=0) # Add one dimension to transform our array into a batch
--- sandbox/models.py
+++ sandbox/models.py
@@ -6,19 +6,22 @@ import gradcam
 from classification_models.tfkeras import Classifiers
 import tensorflow_addons as tfa
 import tensorflow as tf
-
-def build_seresnext():
+def factory(config):
+    if(config.model_name=="seresnext50"):
+        return build_seresnext(config)
+    
+def build_seresnext(config):
         
     SEResNEXT50, _ = Classifiers.get('seresnext50')
-    base_model = SEResNEXT50(input_shape=(gradcam.config.img_size*int(gradcam.config.num_tiles**0.5), \
-                                          gradcam.config.img_size*int(gradcam.config.num_tiles**0.5), 3), \
+    base_model = SEResNEXT50(input_shape=(config.img_size*int(config.num_tiles**0.5), \
+                                          config.img_size*int(config.num_tiles**0.5), 3), \
                              weights='imagenet', include_top=False)
     
     x = GlobalAveragePooling2D()(base_model.output)
-    output = Dense(gradcam.config.num_classes, activation='softmax')(x)
+    output = Dense(config.num_classes, activation='softmax')(x)
     model = Model(inputs=[base_model.input], outputs=[output])
 
-    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=gradcam.config.learning_rate), \
+    model.compile(loss='categorical_crossentropy', optimizer=Adam(lr=config.learning_rate), \
               metrics=[tfa.metrics.CohenKappa(weightage='quadratic', num_classes=6)])
     
     return model
