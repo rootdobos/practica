0d49817e141925e9f6357f9184f5e044d62ae3f6
rootdobos <49080519+rootdobos@users.noreply.github.com>
2023-03-13 20:09:30 +0200

    added config to tile extractor for easier pipeline

--- sandbox/tile_extraction_v3.py
+++ sandbox/tile_extraction_v3.py
@@ -12,137 +12,139 @@ import cv2
 import utils3
 import random
 IMG_DIR = 'train_images/'
-
-AUGMENTATION=4
-LAYER = 1
-SIZE = 128
-COLS = 5
-ROWS = 5
-N = COLS*ROWS
 TILES_BASE_DIR="tiles/"
-TILES_DIR="{}{}_{}_{}_{}".format(TILES_BASE_DIR,SIZE,COLS,ROWS,AUGMENTATION)
-def process_image(idx):    
+class TileExtractor:
+    def __init__(self, layer, size, cols,rows,augmentation):
+        self.layer = layer
+        self.size = size
+        self.cols=cols
+        self.rows=rows
+        self.n=cols*rows
+        self.augmentation=augmentation
+        self.tiles_dir="{}{}_{}_{}_{}".format(TILES_BASE_DIR,self.size,self.cols,self.rows,self.augmentation)
 
-    im = utils3.imread(os.path.join(IMG_DIR, f"{idx}.tiff"), layer=LAYER)
-    im = np.asarray(im)
-    tiles = akensert_tiles(im)
-    im = join_tiles(tiles)
-    im = Image.fromarray(im)
-    im.save(os.path.join(TILES_DIR, f"{idx}.png"), format='PNG', quality=90)
-    
-    for i in range(AUGMENTATION):
-        np.random.shuffle(tiles)
-        for j in range(N):
-            rand=random.randrange(0,4)
-            if(rand==1):
-                tiles[j]= cv2.rotate(tiles[j],cv2.ROTATE_90_CLOCKWISE)
-            if(rand==2):
-                tiles[j]= cv2.rotate(tiles[j],cv2.ROTATE_90_COUNTERCLOCKWISE)
-            if(rand==3):
-                tiles[j]= cv2.rotate(tiles[j],cv2.ROTATE_180)
-        im = join_tiles(tiles)
+    def process_image(self,idx):    
+
+        im = utils3.imread(os.path.join(IMG_DIR, f"{idx}.tiff"), layer=self.layer)
+        im = np.asarray(im)
+        tiles = self.akensert_tiles(im)
+        im = self.join_tiles(tiles)
         im = Image.fromarray(im)
-        im.save(os.path.join(TILES_DIR, f"{idx}_{i}.png"), format='PNG', quality=90)
+        im.save(os.path.join(self.tiles_dir, f"{idx}.png"), format='PNG', quality=90)
+        
+        for i in range(self.augmentation):
+            np.random.shuffle(tiles)
+            for j in range(self.n):
+                rand=random.randrange(0,4)
+                if(rand==1):
+                    tiles[j]= cv2.rotate(tiles[j],cv2.ROTATE_90_CLOCKWISE)
+                if(rand==2):
+                    tiles[j]= cv2.rotate(tiles[j],cv2.ROTATE_90_COUNTERCLOCKWISE)
+                if(rand==3):
+                    tiles[j]= cv2.rotate(tiles[j],cv2.ROTATE_180)
+            im = self.join_tiles(tiles)
+            im = Image.fromarray(im)
+            im.save(os.path.join(self.tiles_dir, f"{idx}_{i}.png"), format='PNG', quality=90)
 
-def split_tiles(img:np.ndarray)->np.ndarray:
-    reshaped = img.reshape(
-        img.shape[0] // SIZE,
-        SIZE,
-        img.shape[1] // SIZE,
-        SIZE,
-        3,
-    )
-    transposed = reshaped.transpose(0, 2, 1, 3, 4)
-    return transposed.reshape(-1, SIZE, SIZE, 3)
+    # def split_tiles(img:np.ndarray)->np.ndarray:
+    #     reshaped = img.reshape(
+    #         img.shape[0] // SIZE,
+    #         SIZE,
+    #         img.shape[1] // SIZE,
+    #         SIZE,
+    #         3,
+    #     )
+    #     transposed = reshaped.transpose(0, 2, 1, 3, 4)
+    #     return transposed.reshape(-1, SIZE, SIZE, 3)
 
-def join_tiles(img:np.ndarray)->np.ndarray:
-    reshaped = img.reshape(
-        COLS,
-        ROWS,    
-        img.shape[1],
-        img.shape[2],
-        3
-    )
-    transposed = reshaped.transpose(0, 2, 1, 3, 4)
-    return transposed.reshape(COLS * SIZE, ROWS * SIZE, 3)
-
-def lafoss_tiles(img:np.ndarray)->np.ndarray:
-    
-    # calculate paddings
-    H, W, _ = img.shape
-    pad_w = (SIZE - W % SIZE) % SIZE
-    pad_h = (SIZE - H % SIZE) % SIZE
-    
-    # implement padding
-    padded = np.pad(
-        img,
-        [[pad_h // 2, pad_h - pad_h // 2],
-         [pad_w // 2, pad_w - pad_w // 2],
-         [0, 0]],
-        constant_values=255, # 255 - white
-    )
-    
-    # split image into tiles
-    tiles = split_tiles(padded)
-    
-    # calculate sums of all pixsels for each tile
-    sums = tiles.reshape(tiles.shape[0], -1).sum(axis=-1)
-    
-    # take top N tiles by minimum sum value
-    idxs_selected = np.argsort(sums)[:N]
-    selected = tiles[idxs_selected]
-    
-    # append white tiles if necessary
-    if len(selected)<N:
-        selected = np.pad(
-            selected,
-            [[0,N-len(selected)],[0,0],[0,0],[0,0]],
-            constant_values=255
+    def join_tiles(self,img:np.ndarray)->np.ndarray:
+        reshaped = img.reshape(
+            self.cols,
+            self.rows,    
+            img.shape[1],
+            img.shape[2],
+            3
         )
-    
-    # join selected tiles into one image
-    merged = join_tiles(selected)
+        transposed = reshaped.transpose(0, 2, 1, 3, 4)
+        return transposed.reshape(self.cols * self.size, self.rows * self.size, 3)
 
-    return merged
+    # def lafoss_tiles(img:np.ndarray)->np.ndarray:
+        
+    #     # calculate paddings
+    #     H, W, _ = img.shape
+    #     pad_w = (SIZE - W % SIZE) % SIZE
+    #     pad_h = (SIZE - H % SIZE) % SIZE
+        
+    #     # implement padding
+    #     padded = np.pad(
+    #         img,
+    #         [[pad_h // 2, pad_h - pad_h // 2],
+    #         [pad_w // 2, pad_w - pad_w // 2],
+    #         [0, 0]],
+    #         constant_values=255, # 255 - white
+    #     )
+        
+    #     # split image into tiles
+    #     tiles = split_tiles(padded)
+        
+    #     # calculate sums of all pixsels for each tile
+    #     sums = tiles.reshape(tiles.shape[0], -1).sum(axis=-1)
+        
+    #     # take top N tiles by minimum sum value
+    #     idxs_selected = np.argsort(sums)[:N]
+    #     selected = tiles[idxs_selected]
+        
+    #     # append white tiles if necessary
+    #     if len(selected)<N:
+    #         selected = np.pad(
+    #             selected,
+    #             [[0,N-len(selected)],[0,0],[0,0],[0,0]],
+    #             constant_values=255
+    #         )
+        
+    #     # join selected tiles into one image
+    #     merged = join_tiles(selected)
 
-def akensert_tiles(img:np.ndarray, debug=False)->np.ndarray:    
-    # get tile coords
-    img, coords = utils3.compute_coords(
-        img,
-        patch_size=SIZE,
-        precompute=False, # returns new padded img
-        min_patch_info=0.35,
-        min_axis_info=0.35,
-        min_consec_axis_info=0.35,
-        min_decimal_keep=0.7)
-    # sort coords (high info -> low info)
-    coords = sorted(coords, key= lambda x: x[0], reverse=False)
-    
-    # select top N tiles
-    tiles = []
-    for i in range(len(coords)):
-        if i == N:
-            break;
-        _, x, y = coords[i]
-        tiles.append(img[x:x+SIZE,y:y+SIZE])
-    
-    # append white tiles if necessary
-    selected = np.array(tiles)
-    if len(selected)<N:
-        selected = np.pad(
-            selected,
-            [[0,N-len(selected)],[0,0],[0,0],[0,0]],
-            constant_values=255
-        )
-    return selected
-    # merge tiles to one image
-    merged = join_tiles(selected)
-    
-    if debug:
-        for (v, y, x) in coords:
-            img = cv2.rectangle(img, (x, y), (x+SIZE, y+SIZE), color=(0, 0, 0), thickness=5)
-            img = cv2.circle(img, (x, y), radius=5, color=(255, 0, 0), thickness=-1)
-            img = cv2.circle(img, (x+SIZE, y+SIZE), radius=5, color=(0, 255, 0), thickness=-1)
-        return merged, img
-    else:
-        return merged
+    #     return merged
+
+    def akensert_tiles(self,img:np.ndarray, debug=False)->np.ndarray:    
+        # get tile coords
+        img, coords = utils3.compute_coords(
+            img,
+            patch_size=self.size,
+            precompute=False, # returns new padded img
+            min_patch_info=0.35,
+            min_axis_info=0.35,
+            min_consec_axis_info=0.35,
+            min_decimal_keep=0.7)
+        # sort coords (high info -> low info)
+        coords = sorted(coords, key= lambda x: x[0], reverse=False)
+        
+        # select top N tiles
+        tiles = []
+        for i in range(len(coords)):
+            if i == self.n:
+                break;
+            _, x, y = coords[i]
+            tiles.append(img[x:x+self.size,y:y+self.size])
+        
+        # append white tiles if necessary
+        selected = np.array(tiles)
+        if len(selected)<self.n:
+            selected = np.pad(
+                selected,
+                [[0,self.n-len(selected)],[0,0],[0,0],[0,0]],
+                constant_values=255
+            )
+        return selected
+        # merge tiles to one image
+        merged = join_tiles(selected)
+        
+        if debug:
+            for (v, y, x) in coords:
+                img = cv2.rectangle(img, (x, y), (x+SIZE, y+SIZE), color=(0, 0, 0), thickness=5)
+                img = cv2.circle(img, (x, y), radius=5, color=(255, 0, 0), thickness=-1)
+                img = cv2.circle(img, (x+SIZE, y+SIZE), radius=5, color=(0, 255, 0), thickness=-1)
+            return merged, img
+        else:
+            return merged
