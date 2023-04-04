f179b480feacdf3fcb65c6e1af84ab5ea35aba3b
rootdobos <49080519+rootdobos@users.noreply.github.com>
2023-04-04 16:15:15 +0300

    added data analysis

---
+++ sandbox/data_analysis.py
@@ -0,0 +1,148 @@
+import os
+
+import openslide
+import skimage.io
+import random
+import seaborn as sns
+import cv2
+
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import PIL
+from IPython.display import Image, display
+
+import plotly.graph_objs as go
+
+BASE_PATH = 'E:/data/prostate_cancer'
+
+data_dir = f'{BASE_PATH}/train_images'
+mask_dir = f'{BASE_PATH}/train_label_masks'
+
+
+def plot_count(df, feature, title='', size=2):
+    f, ax = plt.subplots(1,1, figsize=(4*size,3*size))
+    total = float(len(df))
+    sns.countplot(df[feature],order = df[feature].value_counts().index, palette='Set2')
+    plt.title(title)
+    for p in ax.patches:
+        height = p.get_height()
+        ax.text(p.get_x()+p.get_width()/2.,
+                height + 3,
+                '{:1.2f}%'.format(100*height/total),
+                ha="center") 
+    plt.show()
+
+
+
+def plot_relative_distribution(df, feature, hue, title='', size=2):
+    f, ax = plt.subplots(1,1, figsize=(4*size,3*size))
+    total = float(len(df))
+    sns.countplot(x=feature, hue=hue, data=df, palette='Set2')
+    plt.title(title)
+    for p in ax.patches:
+        height = p.get_height()
+        ax.text(p.get_x()+p.get_width()/2.,
+                height + 3,
+                '{:1.2f}%'.format(100*height/total),
+                ha="center") 
+    plt.show()
+
+def display_images(slides,train): 
+    f, ax = plt.subplots(5,3, figsize=(18,22))
+    for i, slide in enumerate(slides):
+        image = openslide.OpenSlide(os.path.join(data_dir, f'{slide}.tiff'))
+        spacing = 1 / (float(image.properties['tiff.XResolution']) / 10000)
+        patch = image.read_region((1780,1950), 0, (256, 256))
+        ax[i//3, i%3].imshow(patch) 
+        image.close()       
+        ax[i//3, i%3].axis('off')
+        
+        image_id = slide
+        data_provider = train.loc[slide, 'data_provider']
+        isup_grade = train.loc[slide, 'isup_grade']
+        gleason_score = train.loc[slide, 'gleason_score']
+        ax[i//3, i%3].set_title(f"ID: {image_id}\nSource: {data_provider} ISUP: {isup_grade} Gleason: {gleason_score}")
+
+    plt.show() 
+def display_masks(slides,train): 
+    f, ax = plt.subplots(5,3, figsize=(18,22))
+    for i, slide in enumerate(slides):
+        
+        mask = openslide.OpenSlide(os.path.join(mask_dir, f'{slide}_mask.tiff'))
+        mask_data = mask.read_region((0,0), mask.level_count - 1, mask.level_dimensions[-1])
+        cmap = matplotlib.colors.ListedColormap(['black', 'gray', 'green', 'yellow', 'orange', 'red'])
+
+        ax[i//3, i%3].imshow(np.asarray(mask_data)[:,:,0], cmap=cmap, interpolation='nearest', vmin=0, vmax=5) 
+        mask.close()       
+        ax[i//3, i%3].axis('off')
+        
+        image_id = slide
+        data_provider = train.loc[slide, 'data_provider']
+        isup_grade = train.loc[slide, 'isup_grade']
+        gleason_score = train.loc[slide, 'gleason_score']
+        ax[i//3, i%3].set_title(f"ID: {image_id}\nSource: {data_provider} ISUP: {isup_grade} Gleason: {gleason_score}")
+        f.tight_layout()
+        
+    plt.show()
+
+def load_and_resize_image(img_id):
+    """
+    Edited from https://www.kaggle.com/xhlulu/panda-resize-and-save-train-data
+    """
+    biopsy = skimage.io.MultiImage(os.path.join(data_dir, f'{img_id}.tiff'))
+    return cv2.resize(biopsy[-1], (512, 512))
+
+def load_and_resize_mask(img_id):
+    """
+    Edited from https://www.kaggle.com/xhlulu/panda-resize-and-save-train-data
+    """
+    biopsy = skimage.io.MultiImage(os.path.join(mask_dir, f'{img_id}_mask.tiff'))
+    return cv2.resize(biopsy[-1], (512, 512))[:,:,0]
+
+def overlay_mask_on_slide(images,train, center='radboud', alpha=0.8, max_size=(800, 800)):
+    """Show a mask overlayed on a slide."""
+    f, ax = plt.subplots(5,3, figsize=(18,22))
+    
+    
+    for i, image_id in enumerate(images):
+        slide = openslide.OpenSlide(os.path.join(data_dir, f'{image_id}.tiff'))
+        mask = openslide.OpenSlide(os.path.join(mask_dir, f'{image_id}_mask.tiff'))
+        slide_data = slide.read_region((0,0), slide.level_count - 1, slide.level_dimensions[-1])
+        mask_data = mask.read_region((0,0), mask.level_count - 1, mask.level_dimensions[-1])
+        mask_data = mask_data.split()[0]
+        
+        
+        # Create alpha mask
+        alpha_int = int(round(255*alpha))
+        if center == 'radboud':
+            alpha_content = np.less(mask_data.split()[0], 2).astype('uint8') * alpha_int + (255 - alpha_int)
+        elif center == 'karolinska':
+            alpha_content = np.less(mask_data.split()[0], 1).astype('uint8') * alpha_int + (255 - alpha_int)
+
+        alpha_content = PIL.Image.fromarray(alpha_content)
+        preview_palette = np.zeros(shape=768, dtype=int)
+
+        if center == 'radboud':
+            # Mapping: {0: background, 1: stroma, 2: benign epithelium, 3: Gleason 3, 4: Gleason 4, 5: Gleason 5}
+            preview_palette[0:18] = (np.array([0, 0, 0, 0.5, 0.5, 0.5, 0, 1, 0, 1, 1, 0.7, 1, 0.5, 0, 1, 0, 0]) * 255).astype(int)
+        elif center == 'karolinska':
+            # Mapping: {0: background, 1: benign, 2: cancer}
+            preview_palette[0:9] = (np.array([0, 0, 0, 0, 1, 0, 1, 0, 0]) * 255).astype(int)
+
+        mask_data.putpalette(data=preview_palette.tolist())
+        mask_rgb = mask_data.convert(mode='RGB')
+        overlayed_image = PIL.Image.composite(image1=slide_data, image2=mask_rgb, mask=alpha_content)
+        overlayed_image.thumbnail(size=max_size, resample=0)
+
+        
+        ax[i//3, i%3].imshow(overlayed_image) 
+        slide.close()
+        mask.close()       
+        ax[i//3, i%3].axis('off')
+        
+        data_provider = train.loc[image_id, 'data_provider']
+        isup_grade = train.loc[image_id, 'isup_grade']
+        gleason_score = train.loc[image_id, 'gleason_score']
+        ax[i//3, i%3].set_title(f"ID: {image_id}\nSource: {data_provider} ISUP: {isup_grade} Gleason: {gleason_score}")
